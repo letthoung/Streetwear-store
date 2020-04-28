68149a3505e0d73b7cb4a4c939fbd6d605af0c7c
Thanh Le <tle@2ndsource.com>
2020-04-28 13:51:31 -0400

    Added CollectionPage component

--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import CollectionOverview from '../../components/collection-overview/collection-overview.component';
+import CollectionPage from '../collection/collection.component';
 
 const ShopPage = ({ match }) => {
-
     return (<div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Switch>
+            <Route exact path={`${match.path}`} component={CollectionOverview} />
+            <Route path={`${match.path}/:CollectionId`} component={CollectionPage} />
+        </Switch> 
     </div>);
     
 }
